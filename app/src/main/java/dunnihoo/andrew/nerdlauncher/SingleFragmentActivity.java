8eba103380f527bbf02712c0ebe17ae4c1367b35
andrew <its_me2977@yahoo.com>
2016-08-08 09:48:08 -0700

    Initial commit

---
+++ app/src/main/java/dunnihoo/andrew/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,28 @@
+package dunnihoo.andrew.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by andrew on 8/8/2016.
+ */
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+}
